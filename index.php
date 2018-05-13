0d15d7936b2381c42a2e337823dbd40c204c0bf9
Ulrich Pogson <grapplerulrich@gmail.com>
2018-05-13 13:10:43 +0200

    Add support for specific bot and sleeping

--- index.php
+++ index.php
@@ -10,12 +10,14 @@ Debugger::enable();
  * - get token from https://api.slack.com/methods/im.history/test
  * - get channel name from https://api.slack.com/methods/im.list/test
  */
-$channelName = '<CHANNEL-NAME>';
-$token = '<GENERATED-TOKEN>';
-$deletedCountInOneCall = 1000;
+ $channelName = '<CHANNEL-NAME>';
+ $token = '<GENERATED-TOKEN>';
+$botID = '<BOT-ID>';
+$deletedCountInOneCall = 9000;
+$messagesCount = 100;
 
 // slack can get only 100 last messages in one call
-for($i = 1; $i <= ($deletedCountInOneCall / 100); $i++)
+for($i = 1; $i <= ($deletedCountInOneCall / $messagesCount); $i++)
 {
 	// delete messages from channel
 	$messagesUrl = "https://slack.com/api/channels.history?token=$token&channel=$channelName&count=$messagesCount&pretty=1";
@@ -24,7 +26,6 @@ for($i = 1; $i <= ($deletedCountInOneCall / 100); $i++)
 	// $messagesUrl = "https://slack.com/api/im.history?token=$token&channel=$channelName&count=$messagesCount&pretty=1";
 
     // get messages
-    $messagesCount = 100;
     $messagesJson = file_get_contents($messagesUrl);
     $messages = json_decode($messagesJson);
 
@@ -32,6 +33,9 @@ for($i = 1; $i <= ($deletedCountInOneCall / 100); $i++)
     $ts = [];
     if (isset($messages->messages)) {
         foreach($messages->messages as $m) {
+			if ( $botID !== $m->bot_id ) {
+				continue;
+			}
             $ts[] = $m->ts;
         }
     }
@@ -40,10 +44,24 @@ for($i = 1; $i <= ($deletedCountInOneCall / 100); $i++)
     $deleteUrl = "https://slack.com/api/chat.delete?token=$token&channel=$channelName&pretty=1&ts=";
 
     // delete all messages
+	$ts_count = 0;
     foreach ($ts as $t) {
+		$ts_count++;
         $r = file_get_contents($deleteUrl . $t);
-    }
+		if ( ! $r ) {
+			var_dump($ts_count . ' sleep');
+			sleep(3);
+			$r = file_get_contents($deleteUrl . $t);
+		}
+		if ( $r ) {
+			var_dump('ok');
+		} else {
+			var_dump($deleteUrl . $t);
+		}
 
+    }
+	var_dump('batch sleep');
+	sleep(3);
     echo "deleted" . sizeof($ts) . " messages" . "\n";
 }
 
